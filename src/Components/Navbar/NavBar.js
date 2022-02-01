99c9e7d2cd8a2dcaaf85737d67b80df8d1590ab3
Quentin Bourgeois <quentintgc@live.fr>
2022-02-01 23:18:49 +0100

    burger nav

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -8,27 +8,35 @@ export default function NavBar() {
   
   const [showLinks, setShowLinks] = useState(false)
 
+  const nav = document.querySelector('.navbar')
+
   const handleShowLinks = () => {
     setShowLinks(!showLinks)
   }
 
+  const closeNav = () => {
+    if (showLinks){
+      nav.classList.remove('show-nav')
+    }
+  }
+
   return (
-    <nav className='navbar showNav'>
+    <nav className={`navbar ${showLinks ? 'show-nav' : 'hide-nav'}`}>
       <img src={logo} alt="logo du site" className='navbar-logo' />
       <ul className='navbar-links'>
-        <li className='navbar-item'>
+        <li className='navbar-item slideAnime-1' onClick={closeNav}>
           <Link className='navbar-link' to="/caroline-chaussin/">Accueil</Link>
         </li>
-        <li className='navbar-item'>
+        <li className='navbar-item slideAnime-2' onClick={closeNav}>
           <Link className='navbar-link' to="/caroline-chaussin/hypno">Hypnothérapie</Link>
         </li>
-        <li className='navbar-item'>
+        <li className='navbar-item slideAnime-3' onClick={closeNav}>
           <Link className='navbar-link' to="/caroline-chaussin/carto">Cartomencie</Link>
         </li>
-        <li className='navbar-item'>
+        <li className='navbar-item slideAnime-4' onClick={closeNav}>
           <Link className='navbar-link' to="/caroline-chaussin/atelier">Les Ateliers</Link>
         </li>
-        <li className='navbar-item'>
+        <li className='navbar-item slideAnime-5' onClick={closeNav}>
           <Link className='navbar-link' to="/caroline-chaussin/ethique">Mon Ethique</Link>
         </li>
       </ul>
